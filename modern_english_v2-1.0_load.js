3a4dc5f2435bfa539f7d0feefc3e3dbb67ec6d9c
Justin Eberlein <jmeberlein@gmail.com>
2018-04-25 11:50:35 -0500

    Bugfix

--- modern_english_v2-1.0_load.js
+++ modern_english_v2-1.0_load.js
@@ -1,1 +1,1 @@
-KeymanWeb.KRS(new Stub_Keyboard_modern_english_v2()); function Stub_Keyboard_modern_english_v2() {this.KF="modern_english_v2-1.0.js";this.KI="Keyboard_modern_english_v2";this.KN="Modern English v2";}
+KeymanWeb.KRS(new Stub_Keyboard_modern_english_v2()); function Stub_Keyboard_modern_english_v2() {this.KF="modern_english_v2-1.0.js";this.KI="Keyboard_modern_english_v2";this.KN="Sindarin";}
